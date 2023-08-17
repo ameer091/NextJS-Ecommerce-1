fd11b09fab7489e54770f8d725401ae8c47bb35b
Ameer Fields <ameersf0@gmail.com>
2023-08-17 02:32:32 -0400

    Recommented correctly

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,9 +1,9 @@
-{/*This is where I first used and added multiparty from yarnpkg */}
+// {/*This is where I first used and added multiparty from yarnpkg */}
 import multiparty from "multiparty";
-{/* This is where I connected to the S3 bucket that I created using aws.  The package that add to the next project is yarn add @aws-sdk/client-s3 */ }
+// {/* This is where I connected to the S3 bucket that I created using aws.  The package that add to the next project is yarn add @aws-sdk/client-s3 */ }
 import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3"
 import fs from 'fs'
-{/* mime is what we are using to check the type of media file we are dealing with.  Specicially in this case, we are checking to see what type of image file we are dealing with */}
+// {/* mime is what we are using to check the type of media file we are dealing with.  Specicially in this case, we are checking to see what type of image file we are dealing with */}
 import mime from 'mime-types'
 import {mongooseConnect} from "@/lib/mongoose"
 import {getServerSession} from "next-auth"
@@ -11,7 +11,7 @@ import {isAdminRequest} from "@/pages/api/auth/[...nextauth]"
 
 const bucketName = 'ameer-nextjs-ecommerce'
 
-{/*This is also where I first used AWS S3 buckets */}
+// {/*This is also where I first used AWS S3 buckets */}
 
 export default async function handle(req, res) {
   await mongooseConnect()
@@ -32,13 +32,13 @@ export default async function handle(req, res) {
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     },
   })
-  {/* The const links is in case we are dealing with multiple links, we need somewhere to push them all */}
+  // {/* The const links is in case we are dealing with multiple links, we need somewhere to push them all */}
   const links = [];
-  {/*The following for loop is so that we can change the file name from the original name of all of the images that we get from the upload.  The reason we do this is because we dont want the file name to be the same because it can cause errors, so we are changing the name of what is uploaded to be something random whenever it is uploaded to avoid the issue */ }
+  // {/*The following for loop is so that we can change the file name from the original name of all of the images that we get from the upload.  The reason we do this is because we dont want the file name to be the same because it can cause errors, so we are changing the name of what is uploaded to be something random whenever it is uploaded to avoid the issue */ }
   for (const file of files.file) {
-    {/* The following const is to find the extension of the file, so to look for .jpg, .jpeg, etc*/ }
+    // {/* The following const is to find the extension of the file, so to look for .jpg, .jpeg, etc*/ }
     const ext = file.originalFilename.split('.').pop()
-    {/* The following is to find the date of when the file is uploaded and then create a string based on the date.  There are other options that we could try, but this should ensure that the string that is created is different every time because the time and date should not be the same.  You can add more to make it more unique, but I believe that this should be fine*/}
+    // {/* The following is to find the date of when the file is uploaded and then create a string based on the date.  There are other options that we could try, but this should ensure that the string that is created is different every time because the time and date should not be the same.  You can add more to make it more unique, but I believe that this should be fine*/}
     const newFileName = Date.now() + '.' + ext
     await client.send(new PutObjectCommand({
       Bucket: bucketName,
@@ -55,7 +55,7 @@ export default async function handle(req, res) {
   return res.json({links})
 }
 
-{/* We are telling it not to parse our request, we want to parse it ourselves */}
+// {/* We are telling it not to parse our request, we want to parse it ourselves */}
 
 export const config = {
   api: {bodyParser:false},
